4dff43bb0665fc1426f1584b8145673a82d02e96
eilon <eilon159123@gmail.com>
2022-05-01 14:02:03 +0300

    update

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Eilon1212',
+    password: '',
     database: 'vacation'
 });
 
